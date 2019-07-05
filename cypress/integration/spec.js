a16061245713b41ab3cd58846b6347decdab7d84
Josef Biehler <jbiehler@samhammer.de>
2019-07-05 14:56:56 +0200

    create large test names

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,20 @@
 describe('page', () => {
-  it('works', () => {})
+	
+	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var pathLength = 1000;
+   
+   var name1 = "";
+   var name2 = "";
+   
+   var fn = () => {
+	   var result = "";
+	   for(var i = 0; i < pathLength; i++) {
+		   result += characters.charAt(Math.floor(Math.random() * characters.length));
+	   }
+	   return result;
+   }
+   
+  it(fn(), () => { asert(false).toBe(true); })
+  
+  it(fn(), () => { asert(false).toBe(true); })
 })
